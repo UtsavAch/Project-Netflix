0e6d52458303d7a8ec870224e05427481dfee97f
André <andre.adrego@gmail.com>
2024-12-03 10:47:37 +0000

    theme colors works correctly now

--- app/src/main/java/com/example/movieapp/AppNavigations.kt
+++ app/src/main/java/com/example/movieapp/AppNavigations.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
@@ -1,5 +1,7 @@
 package com.example.movieapp.ui.theme
+import android.app.Activity
 import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
@@ -8,8 +10,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -267,6 +273,7 @@ fun AppTheme(
       darkTheme -> darkScheme
       else -> lightScheme
   }
+    Log.d("AppTheme", "Using darkTheme: $darkTheme, dynamicColor: $dynamicColor")
 
   MaterialTheme(
     colorScheme = colorScheme,
